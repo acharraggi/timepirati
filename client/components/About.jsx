230ec1a7a52cdf98ccc284ca30410614c05b9576
Mike Silversides <mike.silversides@gmail.com>
2017-08-17 09:38:29 -0700

    WIP: added a counterStore on the about page, created a rootStore to hold all the stores. Got counter and timer working together.

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-//import styles from './Timer.css';
 import {observer} from "mobx-react";
 
 export default @observer class About extends React.Component {
   constructor(props) {
     super(props);
+    this.counterStore = props.rootStore.counterStore;
   }
 
   render() {
@@ -12,7 +12,14 @@ export default @observer class About extends React.Component {
         <div>
           <h2>About</h2>
           <p>this is the about page.</p>
+          <hr/>
+          <h2>Counter test</h2>
+          <button onClick={() => this.counterStore.increment()} >
+            increment
+          </button>
+          <p>count: {this.counterStore.mainDisplay}</p>
         </div>
     );
   }
 }
+// {this.counterStore.mainDisplay()}
